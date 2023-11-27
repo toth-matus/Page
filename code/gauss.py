039da9b046a03835f437e66b85a59d6077ade07f
toth-matus <152110084+toth-matus@users.noreply.github.com>
2023-11-27 23:24:10 +0100

    Update gauss.py

--- code/gauss.py
+++ code/gauss.py
@@ -1,26 +1,22 @@
-x = int(input("Pocet stlpcov aj s |b"))
-y = int(input("Pocet riadkov"))
-pole=[x]*y
-
-def hladaj(p):
+def hladaj(p):#Finding non 0 element
     if pole[p][p]==0:
         for i in range(0,y):
             if pole[i][p]!=0:
                 for j in range(p,x):
                     pole[p][j],pole[i][j] = pole[i][j],pole[p][j]
-def hore(p):
+def hore(p):#Up Elimination
     for i in range(p):
         if pole[p][p]!=0:
             pom = pole[i][p]/pole[p][p]
             for j in range(p,x):
                 pole[i][j]=pole[i][j]-pom*pole[p][j]
-def dole(p):
+def dole(p):#Down Elimination
     for i in range(p+1,y):
         if pole[p][p]!=0:
             pom = pole[i][p]/pole[p][p]
             for j in range(p,x):
                 pole[i][j]=pole[i][j]-pom*pole[p][j]
-def vydel():
+def vydel():#Simplifying Matrice
     for i in range(y):
         dell = pole[i][i]
         if dell !=0:
@@ -29,12 +25,16 @@ def vydel():
                 if abs(pom)<10**(-10):
                     pom=0
                 pole[i][j]=pom
-def vypis():
+def vypis():#Writing Matrice
     for i in range(y):
         print(pole[i])
 
+x = int(input("Number of columns"))
+y = int(input("Number of rows"))
+pole=[x]*y
+
 for i in range(y):
-    pom = input(f"zadaj {i+1}. riadok aj s |b")
+    pom = input(f"Write {i+1}. row")
     pole[i] = pom.split()
 
 for i in range(y):
